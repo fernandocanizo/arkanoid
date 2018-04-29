cb34fe2077825d5a4c22473312e4ccf312fd765b
Fernando L. Canizo <fernandocanizo@gmail.com>
2018-04-29 18:42:48 -0300

    Make the ball a global object
    Also split updater functions from drawing ones.

--- game.js
+++ game.js
@@ -4,20 +4,51 @@
 
 const FPS = 30;
 
-const updateAll = (canvas, context) => {
+const ball = {
+  radius: 10,
+  position: {
+    x: 100,
+    y: 100,
+  },
+  velocity: {
+    x: 5,
+    y: 5,
+  },
+  color: 'white',
+};
+
+const drawBackground = (canvas, context) => {
   context.fillStyle = 'black';
   context.fillRect(0, 0, canvas.width, canvas.height);
+};
 
-  context.fillStyle = 'white';
+const updateBall = () => {
+};
+
+const drawBall = (canvas, context) => {
+  context.fillStyle = ball.color;
   context.beginPath();
-  context.arc(100, 100, 10, 0, Math.PI * 2, true);
+  context.arc(ball.position.x, ball.position.y, ball.radius, 0, Math.PI * 2);
   context.fill();
 };
+
+const updateAll = () => {
+  updateBall();
+};
+
+const drawAll = (canvas, context) => {
+  drawBackground(canvas, context);
+  drawBall(canvas, context);
+};
+
 
 window.onload = () => {
   const canvas = document.getElementById('playGround');
   const context = canvas.getContext('2d');
 
-  const intervalId = setInterval(() => updateAll(canvas, context), 1000 / FPS);
-  console.log(intervalId);
+  const intervalId = setInterval(() => {
+    updateAll(canvas, context);
+    drawAll(canvas, context);
+  }, 1000 / FPS);
+  console.log('intervalId', intervalId);
 };
